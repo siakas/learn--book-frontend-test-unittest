07bf645a52127d931f29c162cfb57850de4c80eb
Sanshiroh SAKAI <sansyo@gmail.com>
2024-12-08 06:46:16 +0900

    Update: Vitest 設定更新

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { coverageConfigDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./vitest-setup.ts"],
-    include: ["src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+
+    // テスト対象を定義
+    include: ["./src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+
+    // カバレッジレポートから除外したいファイルを指定（include / exlude とは別に要設定）
+    coverage: {
+      exclude: [...coverageConfigDefaults.exclude, "**/_sample/**"],
+    },
   },
 });
